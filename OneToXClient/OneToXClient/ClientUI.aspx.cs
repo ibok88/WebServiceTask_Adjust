527c3cc9185a00904fd8dd7b4720e83e6de8659d
ibok <princeibok@gmail.com>
2018-08-19 01:46:28 +0200

    Return JsonArray from One To X

--- OneToXClient/OneToXClient/ClientUI.aspx.cs
+++ OneToXClient/OneToXClient/ClientUI.aspx.cs
@@ -4,23 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
 
 namespace OneToXClient
 {
+    
     public partial class ClientUI : System.Web.UI.Page
     {
+        
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
+           
         }
 
         protected void BtnGet_Click(object sender, EventArgs e)
         {
             ListOfNumbers.WebServerClient client = new ListOfNumbers.WebServerClient();
+            var jsonSerilaizer = new JavaScriptSerializer();
 
             if (!string.IsNullOrWhiteSpace(TextBox1.Text))
             {
-                Label1.Text = string.Join(",", client.GetJsonArray(Convert.ToInt32(TextBox1.Text)));
+                
+                var json_array = jsonSerilaizer.Serialize(client.GetJsonArray(Convert.ToInt32(TextBox1.Text)));
+
+                Label1.Text = json_array;
 
             }
             else
