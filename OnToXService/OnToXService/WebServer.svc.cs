5dee9cacaa1b63a85566a2bfa7a53d54c932145f
saturn88 <39991981+saturn88@users.noreply.github.com>
2018-08-20 03:02:44 +0200

    Update WebServer.svc.cs

--- OnToXService/OnToXService/WebServer.svc.cs
+++ OnToXService/OnToXService/WebServer.svc.cs
@@ -9,8 +9,6 @@ using System.ServiceModel.Description;
 
 namespace OnToXService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WebServer" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select WebServer.svc or WebServer.svc.cs at the Solution Explorer and start debugging.
     public class WebServer : IWebServer
     {
         
